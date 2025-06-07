cbfc4aad8da5f87904049fa0310c6c579e4d6a76
joelj0el <jhoel.titirico@uab.edu.bo>
2025-06-07 12:07:21 -0400

    Error: Corregir ruta de template en views.py
    Remover prefijo innecesario 'my_first_app/templates/' de la ruta del template.
    
    Asi tambien que antes de hacer correr el proyecto es necesario hacer la migraciones.

--- my_fourth_project/my_first_app/views.py
+++ my_fourth_project/my_first_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def myView(request):
     car_list = [{'title': 'BMW'}, {'title': 'Mazda'}]
     context = {'car_list': car_list}
-    return render(request, 'my_first_app/templates/my_fourth_app/car_list.html', context)
+    return render(request, 'my_fourth_app/car_list.html', context)
